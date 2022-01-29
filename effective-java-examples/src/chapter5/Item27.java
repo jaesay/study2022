4770dfca39396ee5367fab583e9f3785e3d3c054
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-01-29 16:14:15 +0900

    Add Item27

---
+++ effective-java-examples/src/chapter5/Item27.java
@@ -0,0 +1,25 @@
+package chapter5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item27 {
+    // 배열은 covariant
+    Super[] supers = new Sub[] {};
+
+    // 제네릭은 invariant
+    List<Super> superList = new ArrayList<Sub>();
+
+    // 자바에서 제네릭이 공변이 될 경우 문제
+    List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+    List<Object> objects = numbers;
+    objects.add("aaa");
+}
+
+class Sub extends Super {
+
+}
+
+class Super {
+
+}
