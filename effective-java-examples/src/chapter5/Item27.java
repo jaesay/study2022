4ba2512ab28ebeaf642a53faab2c0792f0c9b877
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-01-29 17:06:45 +0900

    Update Item27

--- effective-java-examples/src/chapter5/Item27.java
+++ effective-java-examples/src/chapter5/Item27.java
@@ -8,12 +8,12 @@ public class Item27 {
     Super[] supers = new Sub[] {};
 
     // 제네릭은 invariant
-    List<Super> superList = new ArrayList<Sub>();
+//    List<Super> superList = new ArrayList<Sub>();
 
     // 자바에서 제네릭이 공변이 될 경우 문제
-    List<Integer> numbers = List.of(1, 2, 3, 4, 5);
-    List<Object> objects = numbers;
-    objects.add("aaa");
+//    List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+//    List<Object> objects = numbers;
+//    objects.add("aaa");
 }
 
 class Sub extends Super {
