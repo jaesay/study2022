c6fa76aee42dc5074cfb2443b7826dfa3addee4d
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-01-30 12:52:09 +0900

    메소드 레퍼런스

--- inflearn-java8-example/src/main/java/com/jaesay/chapter1/LambdaExample.java
+++ inflearn-java8-example/src/main/java/com/jaesay/chapter1/LambdaExample.java
@@ -1,13 +1,35 @@
 package com.jaesay.chapter1;
 
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.IntConsumer;
+import java.util.Arrays;
+import java.util.function.*;
 
 public class LambdaExample {
     public static void main(String[] args) {
+        System.out.println("============== 람다 표현식 ==============");
         LambdaExample lambdaExample = new LambdaExample();
         lambdaExample.run();
+
+        System.out.println("============== 메소드 레퍼런스 ==============");
+        // static method를 사용한 method reference
+        UnaryOperator<String> hi = Greeting::hi;
+        System.out.println("hi.apply(\"jaeseong\") = " + hi.apply("jaeseong"));
+
+        // 특정 객체의 인스턴스 메소드 참조
+        Greeting greeting = new Greeting();
+        UnaryOperator<String> hello = greeting::hello;
+        System.out.println("hello.apply(\"jaeseong\") = " + hello.apply("jaeseong"));
+
+        // 생성자 참조
+        Supplier<Greeting> greetingSupplier = Greeting::new;
+        System.out.println("greetingSupplier.get().getClass() = " + greetingSupplier.get().getClass());
+
+        Function<String, Greeting> greetingFunction = Greeting::new;
+        System.out.println("greetingFunction.apply(\"hello\").getClass() = " + greetingFunction.apply("hello").getClass());
+
+        // 임의 객체의 인스턴스 메소드 참조
+        String[] names = {"김이름", "이이름", "박이름"};
+        Arrays.sort(names, String::compareToIgnoreCase); // 배열에 들어있는 임의 객체들이 가지고 있는 메소드 사용
+        System.out.println("Arrays.toString(names) = " + Arrays.toString(names));
     }
 
     private void run() {
