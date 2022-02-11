cfbd1c730556b7fca7d23a0cf2fb083e8b489082
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-02-11 18:12:24 +0900

    Update ParallelStreamExample

--- parallel-asynchronous-using-java/src/main/java/com/jaesay/parallelstream/ParallelStreamExample.java
+++ parallel-asynchronous-using-java/src/main/java/com/jaesay/parallelstream/ParallelStreamExample.java
@@ -4,6 +4,7 @@ import com.jaesay.util.DataSet;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.jaesay.util.CommonUtil.*;
 import static com.jaesay.util.LoggerUtil.log;
@@ -19,10 +20,22 @@ public class ParallelStreamExample {
         timeTaken();
     }
 
-    private List<String> toNameLengthTransform(List<String> namesList) {
+    public List<String> toNameLengthTransform(List<String> namesList) {
         return namesList
 //                .stream()
                 .parallelStream()
+                .map(this::addNameLengthTransform)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> toNameLengthTransform_v2(List<String> namesList, boolean isParallel) {
+        Stream<String> nameStream = namesList.stream();
+
+        if (isParallel) {
+            nameStream.parallel(); // 주로 sequential, parallel 성능 테스트 용도로 사용
+        }
+
+        return nameStream
                 .map(this::addNameLengthTransform)
                 .collect(Collectors.toList());
     }
