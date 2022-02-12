0d2d1d4bdfa9101d1959ee79642d29b4b130b9ed
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-02-12 17:39:58 +0900

    14. Combining Streams and CompletableFuture

--- parallel-asynchronous-using-java/src/main/java/com/jaesay/completablefuture/ProductServiceUsingCompletableFuture.java
+++ parallel-asynchronous-using-java/src/main/java/com/jaesay/completablefuture/ProductServiceUsingCompletableFuture.java
@@ -1,13 +1,14 @@
 package com.jaesay.completablefuture;
 
-import com.jaesay.domain.Product;
-import com.jaesay.domain.ProductInfo;
-import com.jaesay.domain.Review;
+import com.jaesay.domain.*;
+import com.jaesay.service.InventoryService;
 import com.jaesay.service.ProductInfoService;
 import com.jaesay.service.ProductService;
 import com.jaesay.service.ReviewService;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import static com.jaesay.util.CommonUtil.*;
 import static com.jaesay.util.LoggerUtil.log;
@@ -16,12 +17,17 @@ public class ProductServiceUsingCompletableFuture {
 
     private final ProductInfoService productInfoService;
     private final ReviewService reviewService;
+    private final InventoryService inventoryService;
 
-    public ProductServiceUsingCompletableFuture(ProductInfoService productInfoService, ReviewService reviewService) {
+    public ProductServiceUsingCompletableFuture(ProductInfoService productInfoService, ReviewService reviewService, InventoryService inventoryService) {
         this.productInfoService = productInfoService;
         this.reviewService = reviewService;
+        this.inventoryService = inventoryService;
     }
 
+    // productService
+    //      -> a productInfoService call
+    //      -> a reviewService call
     public Product retrieveProductDetails(String productId) {
         startTimer();
 
@@ -42,6 +48,73 @@ public class ProductServiceUsingCompletableFuture {
 
         return productInfoCompletableFuture
                 .thenCombine(reviewCompletableFuture, (productInfo, review) -> new Product(productId, productInfo, review));
+    }
+
+    // productService
+    //      -> a productInfoService call
+    //          -> inventoryService calls
+    //      -> a reviewService call
+    public Product retrieveProductDetailsWithInventory(String productId) {
+        startTimer();
+
+        CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture
+                .supplyAsync(() -> productInfoService.retrieveProductInfo(productId))
+                .thenApply(productInfo -> {
+                    List<ProductOption> productOptions = updateInventory(productInfo); // blocking calls
+                    productInfo.setProductOptions(productOptions); // 동기화 문제는 고려 X
+                    return productInfo;
+                });
+        CompletableFuture<Review> reviewCompletableFuture = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
+
+        Product product = productInfoCompletableFuture
+                .thenCombine(reviewCompletableFuture, (productInfo, review) -> new Product(productId, productInfo, review))
+                .join();
+
+        timeTaken();
+        return product;
+    }
+
+    private List<ProductOption> updateInventory(ProductInfo productInfo) {
+        return productInfo.getProductOptions().stream()
+                .peek(productOption -> {
+                    Inventory inventory = inventoryService.addInventory(productOption);
+                    productOption.setInventory(inventory); // 동기화 문제는 고려 X
+                })
+                .collect(Collectors.toList());
+    }
+
+    public Product retrieveProductDetailsWithInventory_v2(String productId) {
+        startTimer();
+
+        CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture
+                .supplyAsync(() -> productInfoService.retrieveProductInfo(productId))
+                .thenApply(productInfo -> {
+                    List<ProductOption> productOptions = updateInventory_v2(productInfo); // blocking calls
+                    productInfo.setProductOptions(productOptions); // 동기화 문제는 고려 X
+                    return productInfo;
+                });
+        CompletableFuture<Review> reviewCompletableFuture = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
+
+        Product product = productInfoCompletableFuture
+                .thenCombine(reviewCompletableFuture, (productInfo, review) -> new Product(productId, productInfo, review))
+                .join();
+
+        timeTaken();
+        return product;
+    }
+
+    private List<ProductOption> updateInventory_v2(ProductInfo productInfo) {
+        List<CompletableFuture<ProductOption>> completableFutures = productInfo.getProductOptions().stream()
+                .map(productOption -> CompletableFuture.supplyAsync(() -> inventoryService.addInventory(productOption))
+                        .thenApply(inventory -> {
+                            productOption.setInventory(inventory); // 동기화 문제는 고려 X
+                            return productOption;
+                        }))
+                .collect(Collectors.toList());
+
+        return completableFutures.stream()
+                .map(CompletableFuture::join) // 파이프라인에서 한개 씩 blocking 될 것 같지만 위에서 supplyAsync 로 CompletableFuture를 만들때 이미 백그라운드에서 실행
+                .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
--- parallel-asynchronous-using-java/src/test/java/com/jaesay/completablefuture/ProductServiceUsingCompletableFutureTest.java
+++ parallel-asynchronous-using-java/src/test/java/com/jaesay/completablefuture/ProductServiceUsingCompletableFutureTest.java
@@ -1,6 +1,7 @@
 package com.jaesay.completablefuture;
 
 import com.jaesay.domain.Product;
+import com.jaesay.service.InventoryService;
 import com.jaesay.service.ProductInfoService;
 import com.jaesay.service.ReviewService;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,8 @@ class ProductServiceUsingCompletableFutureTest {
 
     ProductInfoService productInfoService = new ProductInfoService();
     ReviewService reviewService = new ReviewService();
-    ProductServiceUsingCompletableFuture service  = new ProductServiceUsingCompletableFuture(productInfoService, reviewService);
+    InventoryService inventoryService = new InventoryService();
+    ProductServiceUsingCompletableFuture service  = new ProductServiceUsingCompletableFuture(productInfoService, reviewService, inventoryService);
 
     @Test
     void retrieveProductDetails() {
@@ -44,6 +46,40 @@ class ProductServiceUsingCompletableFutureTest {
         // then
         assertNotNull(product);
         assertNotNull(product.getProductInfo());
+        assertNotNull(product.getReview());
+    }
+
+    @Test
+    void retrieveProductDetailsWithInventory() {
+        // given
+        String productId = "ABC123";
+
+        // when
+        // 최소 inventory service 실행 시간 * inventory 개수(product option 개수) 만큼 시간이 더 걸린다. (blocking calls)
+        Product product = service.retrieveProductDetailsWithInventory(productId);
+
+        // then
+        assertNotNull(product);
+        assertNotNull(product.getProductInfo());
+        product.getProductInfo().getProductOptions()
+                .forEach(productOption -> assertNotNull(productOption.getInventory()));
+        assertNotNull(product.getReview());
+    }
+
+    @Test
+    void retrieveProductDetailsWithInventory_v2() {
+        // given
+        String productId = "ABC123";
+
+        // when
+        // 최소 inventory service 실행 시간만큼 추가로 더 걸린다.
+        Product product = service.retrieveProductDetailsWithInventory_v2(productId);
+
+        // then
+        assertNotNull(product);
+        assertNotNull(product.getProductInfo());
+        product.getProductInfo().getProductOptions()
+                .forEach(productOption -> assertNotNull(productOption.getInventory()));
         assertNotNull(product.getReview());
     }
 }
