dc08f4406df32481fd09d402884f1fd9f5b8b63b
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-03-27 02:14:19 +0900

    경계 간 매핑하기

--- src/main/java/com/jaesay/buckpal/account/application/port/out/UpdateAccountStatePort.java
+++ src/main/java/com/jaesay/buckpal/account/application/port/out/UpdateAccountStatePort.java
@@ -2,6 +2,7 @@ package com.jaesay.buckpal.account.application.port.out;
 
 import com.jaesay.buckpal.account.domain.Account;
 
+// 매핑하지 않기 전략
 public interface UpdateAccountStatePort {
     void updateActivities(Account account);
 }
