9cf4a8501ff677651b52bbf7e871c35ccfeca436
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-06-08 22:08:21 +0900

    팩토리얼

---
+++ 2. 강의/1. 인프런/coding-test/src/_07_recursive_tree_graph/Main03.java
@@ -0,0 +1,14 @@
+package _07_recursive_tree_graph;
+
+public class Main03 {
+    public static void main(String[] args) {
+        System.out.println("dfs(5) = " + dfs(5));
+    }
+
+    private static int dfs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return dfs(n-1) * n;
+    }
+}
