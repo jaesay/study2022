cb5a1f3d0132a6b409aa5c7b4068a3eef29e04a2
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-06-25 23:18:25 +0900

    2. 회의실 배정

---
+++ 2. 강의/1. 인프런/coding-test/src/_09_greedy/Main02.java
@@ -0,0 +1,58 @@
+package _09_greedy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Meeting> meetings = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            meetings.add(new Meeting(sc.nextInt(), sc.nextInt()));
+        }
+        Main02 main02 = new Main02();
+        System.out.println(main02.solution(meetings));
+    }
+
+    private int solution(List<Meeting> meetings) {
+        Collections.sort(meetings);
+        int maxEnd = Integer.MIN_VALUE;
+        int answer = 0;
+        for (Meeting meeting : meetings) {
+            if (meeting.s >= maxEnd) {
+                maxEnd = meeting.e;
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static class Meeting implements Comparable<Meeting> {
+        int s;
+        int e;
+
+        public Meeting(int s, int e) {
+            this.s = s;
+            this.e = e;
+        }
+
+        @Override
+        public int compareTo(Meeting o) {
+            if (this.e == o.e) {
+                return this.s - o.s;
+            }
+            return this.e - o.e;
+        }
+
+        @Override
+        public String toString() {
+            return "Meeting{" +
+                    "s=" + s +
+                    ", e=" + e +
+                    '}';
+        }
+    }
+}
