73ff805617c84928c06fecce6cff00ad0111656e
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-08 21:23:48 +0900

    23. 상속 포기

--- 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_23_refused_bequest/Employee.java
+++ 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_23_refused_bequest/Employee.java
@@ -1,11 +1,4 @@
 package me.whiteship.refactoring._23_refused_bequest;
 
 public class Employee {
-
-    protected Quota quota;
-
-    protected Quota getQuota() {
-        return new Quota();
-    }
-
 }
