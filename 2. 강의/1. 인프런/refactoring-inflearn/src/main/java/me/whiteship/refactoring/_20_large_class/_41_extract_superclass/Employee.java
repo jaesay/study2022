3193d2698d01dc30e7da272505e5361bbea45541
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-09 11:59:16 +0900

    41. 슈퍼클래 추출하기

--- 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Employee.java
+++ 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Employee.java
@@ -1,26 +1,21 @@
 package me.whiteship.refactoring._20_large_class._41_extract_superclass;
 
-public class Employee {
+public class Employee extends Party {
 
     private Integer id;
 
-    private String name;
-
     private double monthlyCost;
 
-    public double annualCost() {
-        return this.monthlyCost * 12;
+    public Employee(String name) {
+        super(name);
     }
 
     public Integer getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getMonthlyCost() {
+    @Override
+    protected double monthlyCost() {
         return monthlyCost;
     }
 }
---
+++ 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Party.java
@@ -0,0 +1,15 @@
+package me.whiteship.refactoring._20_large_class._41_extract_superclass;
+
+public abstract class Party {
+    protected String name;
+
+    public Party(String name) {
+        this.name = name;
+    }
+
+    public double annualCost() {
+        return this.monthlyCost() * 12;
+    }
+
+    abstract protected double monthlyCost();
+}
