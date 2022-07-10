bb528a591f659b8ad93a4ae08bf9f40562fc0284
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-10 14:57:23 +0900

    14. 매개변수를 질의 함수로 바꾸기

--- 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_14_replace_parameter_with_query/Order.java
+++ 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_14_replace_parameter_with_query/Order.java
@@ -13,11 +13,14 @@ public class Order {
 
     public double finalPrice() {
         double basePrice = this.quantity * this.itemPrice;
-        int discountLevel = this.quantity > 100 ? 2 : 1;
-        return this.discountedPrice(basePrice, discountLevel);
+        return this.discountedPrice(basePrice);
     }
 
-    private double discountedPrice(double basePrice, int discountLevel) {
-        return discountLevel == 2 ? basePrice * 0.90 : basePrice * 0.95;
+    private int discountLevel() {
+        return this.quantity > 100 ? 2 : 1;
+    }
+
+    private double discountedPrice(double basePrice) {
+        return discountLevel() == 2 ? basePrice * 0.90 : basePrice * 0.95;
     }
 }
