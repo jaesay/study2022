b0001a627ae534edfeebb857cdb7f549d5785e1e
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-10 14:38:54 +0900

    21. 서로 다른 인터페이스의 대안 클래스들

--- 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/OrderAlerts.java
+++ 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/OrderAlerts.java
@@ -1,13 +1,16 @@
 package me.whiteship.refactoring._21_alternative_classes_with_different_interfaces;
 
 public class OrderAlerts {
+    private NotificationService notificationService;
 
-    private AlertService alertService;
-    
-    public void alertShipped(Order order) {
-        AlertMessage alertMessage = new AlertMessage();
-        alertMessage.setMessage(order.toString() + " is shipped");
-        alertMessage.setFor(order.getEmail());
-        alertService.add(alertMessage);
+    public OrderAlerts(NotificationService notificationService) {
+        this.notificationService = notificationService;
     }
+
+    public void alertShipped(Order order) {
+        Notification notification = Notification.newNotification(order.toString() + "is shipped")
+                .receiver(order.getEmail());
+        notificationService.sendNotification(notification);
+    }
+
 }
--- 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/OrderProcessor.java
+++ 2. 강의/1. 인프런/refactoring-inflearn/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/OrderProcessor.java
@@ -1,15 +1,17 @@
 package me.whiteship.refactoring._21_alternative_classes_with_different_interfaces;
 
 public class OrderProcessor {
+    private NotificationService notificationService;
 
-    private EmailService emailService;
+    public OrderProcessor(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
 
     public void notifyShipping(Shipping shipping) {
-        EmailMessage emailMessage = new EmailMessage();
-        emailMessage.setTitle(shipping.getOrder() + " is shipped");
-        emailMessage.setTo(shipping.getEmail());
-        emailMessage.setFrom("no-reply@whiteship.com");
-        emailService.sendEmail(emailMessage);
+        Notification notification = Notification.newNotification(shipping.getOrder() + " is shipped")
+                .receiver(shipping.getEmail())
+                .sender("no-reply@whiteship.com");
+        notificationService.sendNotification(notification);
     }
 
 }
