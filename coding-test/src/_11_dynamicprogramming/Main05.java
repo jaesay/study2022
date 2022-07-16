544b7122ff5e8a69250ec2169b2707fc75af4129
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-16 23:21:02 +0900

    5. 동전교환(냅색알고리즘)

---
+++ coding-test/src/_11_dynamicprogramming/Main05.java
@@ -0,0 +1,30 @@
+package _11_dynamicprogramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+        int m = sc.nextInt();
+        Main05 main05 = new Main05();
+        System.out.println(main05.solution(n, m, coins));
+    }
+
+    private int solution(int n, int m, int[] coins) {
+        int[] dy = new int[m + 1]; // i 금액을 만드는데 드는 최소동전개수
+        Arrays.fill(dy, Integer.MAX_VALUE);
+        dy[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j<=m; j++) {
+                dy[j] = Math.min(dy[j], dy[j-coins[i]] + 1);
+            }
+        }
+        return dy[m];
+    }
+}
