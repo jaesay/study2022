172d54dbde36266dbd9ff68906bab1cf702448e2
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-19 21:37:30 +0900

    리팩토링 33. 반복문을 파이프라인으로 바꾸기

--- refactoring-inflearn/src/main/java/me/whiteship/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
+++ refactoring-inflearn/src/main/java/me/whiteship/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
@@ -2,6 +2,8 @@ package me.whiteship.refactoring._13_loop._33_replace_loop_with_pipeline;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Author {
 
@@ -14,16 +16,13 @@ public class Author {
         this.twitterHandle = twitterHandle;
     }
 
+    // https://martinfowler.com/articles/refactoring-pipelines.html
     static public List<String> TwitterHandles(List<Author> authors, String company) {
-        var result = new ArrayList<String> ();
-        for (Author a : authors) {
-            if (a.company.equals(company)) {
-                var handle = a.twitterHandle;
-                if (handle != null)
-                    result.add(handle);
-            }
-        }
-        return result;
+        return authors.stream()
+                .filter(author -> author.company.equals(company))
+                .map(author -> author.twitterHandle)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
 }
