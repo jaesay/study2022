ea919fc943c5a5fb2e4d01c86306ea9f32a1bcc6
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-19 21:26:54 +0900

    냄새 12. 반복되는 switch 문

--- refactoring-inflearn/src/main/java/me/whiteship/refactoring/_12_repeated_swtiches/SwitchImprovements.java
+++ refactoring-inflearn/src/main/java/me/whiteship/refactoring/_12_repeated_swtiches/SwitchImprovements.java
@@ -2,14 +2,13 @@ package me.whiteship.refactoring._12_repeated_swtiches;
 
 public class SwitchImprovements {
 
+    // switch statement -> switch expression
     public int vacationHours(String type) {
-        int result;
-        switch (type) {
-            case "full-time": result = 120;
-            case "part-time": result = 80;
-            case "temporal": result = 32;
-            default: result = 0;
-        }
-        return result;
+        return switch (type) {
+            case "full-time" -> 120;
+            case "part-time" -> 80;
+            case "temporal" -> 32;
+            default -> 0;
+        };
     }
 }
