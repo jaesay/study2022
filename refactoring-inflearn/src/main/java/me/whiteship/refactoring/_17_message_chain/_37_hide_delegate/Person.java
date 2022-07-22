5eac0e73e8a53f5ea69286c00514be18ce959cb8
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-22 18:58:20 +0900

    리팩토링 37. 위임 숨기기

--- refactoring-inflearn/src/main/java/me/whiteship/refactoring/_17_message_chain/_37_hide_delegate/Person.java
+++ refactoring-inflearn/src/main/java/me/whiteship/refactoring/_17_message_chain/_37_hide_delegate/Person.java
@@ -21,4 +21,8 @@ public class Person {
     public void setDepartment(Department department) {
         this.department = department;
     }
+
+    Person getManager() {
+        return getDepartment().getManager();
+    }
 }
