6b15bd4c9148415d7f86a27edd124b35a78f549e
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-30 19:25:08 +0900

    1. 두 배열 합치기(two pointers algorithm)

---
+++ 02. 인프런/05. 자바 알고리즘 문제풀이 - 코딩테스트 대비/coding-test/src/_03_twopointers_slidingwindow/Main01.java
@@ -0,0 +1,51 @@
+package _03_twopointers_slidingwindow;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        Main01 main = new Main01();
+        List<Integer> solution = main.solution(n, arr1, m, arr2);
+        solution.forEach(i -> System.out.print(i + " "));
+    }
+
+    private List<Integer> solution(int n, int[] arr1, int m, int[] arr2) {
+        List<Integer> answer = new ArrayList<>();
+        int p1 = 0;
+        int p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (arr1[p1] <= arr2[p2]) {
+                answer.add(arr1[p1]);
+                p1++;
+            } else {
+                answer.add(arr2[p2]);
+                p2++;
+            }
+        }
+
+        for (int i = p1; i < n; i++) {
+            answer.add(arr1[i]);
+        }
+
+        for (int i = p2; i < m; i++) {
+            answer.add(arr2[i]);
+        }
+
+        return answer;
+    }
+}
