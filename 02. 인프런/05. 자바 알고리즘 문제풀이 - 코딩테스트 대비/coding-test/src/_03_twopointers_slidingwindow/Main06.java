423c9645f7c4f8b2c02431358cfced3cb87dde67
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-31 23:19:53 +0900

    6. 최대 길이 연속부분수열(복합적 문제)

---
+++ 02. 인프런/05. 자바 알고리즘 문제풀이 - 코딩테스트 대비/coding-test/src/_03_twopointers_slidingwindow/Main06.java
@@ -0,0 +1,31 @@
+package _03_twopointers_slidingwindow;
+
+import java.util.Scanner;
+
+public class Main06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Main06 main = new Main06();
+        System.out.println(main.solution(n, k, arr));
+    }
+
+    private int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) cnt++;
+            while (cnt > k) {
+                if (arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt - lt + 1);
+        }
+        return answer;
+    }
+}
