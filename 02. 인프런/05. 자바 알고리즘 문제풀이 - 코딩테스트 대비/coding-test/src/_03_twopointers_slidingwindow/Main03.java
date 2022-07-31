1acea6e0ba66c0faf3b79c1497ad9e305857cc8f
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-31 16:23:49 +0900

    3. 최대 매출(Sliding window)

---
+++ 02. 인프런/05. 자바 알고리즘 문제풀이 - 코딩테스트 대비/coding-test/src/_03_twopointers_slidingwindow/Main03.java
@@ -0,0 +1,33 @@
+package _03_twopointers_slidingwindow;
+
+import java.util.Scanner;
+
+public class Main03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Main03 main = new Main03();
+        System.out.print(main.solution(n, k, arr));
+    }
+
+    private int solution(int n, int k, int[] arr) {
+        int answer = 0;
+        for (int i = 0; i < k; i++) {
+            answer += arr[i];
+        }
+
+        int sum = answer;
+        for (int i = k; i < n; i++) {
+            sum = sum - arr[i - k] + arr[i];
+            answer = Math.max(sum, answer);
+        }
+
+        return answer;
+    }
+}
