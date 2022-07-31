70c6d285d0bfd2d19108ca336943070af0abb819
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-07-31 17:11:54 +0900

    4. 연속부분수열(복합적 문제)

---
+++ 02. 인프런/05. 자바 알고리즘 문제풀이 - 코딩테스트 대비/coding-test/src/_03_twopointers_slidingwindow/Main04.java
@@ -0,0 +1,35 @@
+package _03_twopointers_slidingwindow;
+
+import java.util.Scanner;
+
+public class Main04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Main04 main = new Main04();
+        System.out.println(main.solution(n, m, arr));
+    }
+
+    private int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int sum = 0;
+        int lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            sum += arr[rt];
+            if (sum == m) answer++;
+            while (sum >= m) {
+                sum -= arr[lt++];
+                if (sum == m) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
