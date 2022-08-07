04c43be10d63cd64abb0faca9ed40f40aaf7ce4c
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-08-07 17:04:09 +0900

    2. 버블 정렬

---
+++ 02. 인프런/05. 자바 알고리즘 문제풀이 - 코딩테스트 대비/coding-test/src/_06_sort_searching/Main02.java
@@ -0,0 +1,31 @@
+package _06_sort_searching;
+
+import java.util.Scanner;
+
+public class Main02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Main02 main = new Main02();
+        for (int i : main.solution(n, arr)) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+}
