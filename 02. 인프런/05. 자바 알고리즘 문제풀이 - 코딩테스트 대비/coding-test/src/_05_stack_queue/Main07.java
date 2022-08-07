444963a8ed1afb5d33c86368bdce0ebba4bacfbe
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-08-07 15:23:20 +0900

    7. 교육과정 설계

---
+++ 02. 인프런/05. 자바 알고리즘 문제풀이 - 코딩테스트 대비/coding-test/src/_05_stack_queue/Main07.java
@@ -0,0 +1,37 @@
+package _05_stack_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        Main07 main = new Main07();
+        System.out.println(main.solution(a, b));
+    }
+
+    private String solution(String a, String b) {
+        Queue<Character> q = new LinkedList<>();
+        for (char c : a.toCharArray()) {
+            q.add(c);
+        }
+
+        for (char c : b.toCharArray()) {
+            if (q.contains(c)) {
+                if (c != q.poll()) {
+                    return "NO";
+                }
+            }
+        }
+
+        if (!q.isEmpty()) {
+            return "NO";
+        }
+
+        return "YES";
+    }
+}
