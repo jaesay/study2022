e9e0b5eebd3d31f87595675cd0d3e8becbf2e858
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-09-08 21:23:52 +0900

    refactor: 프로젝트명 안바꾼곳 수정

--- devops-k8s-master/http-spring/src/main/java/com/jaesay/httpspring/HttpSpringApplication.java
+++ devops-k8s-master/http-spring/src/main/java/com/jaesay/httpspring/HttpSpringApplication.java
@@ -1,4 +1,4 @@
-package com.jaesay.demodocker;
+package com.jaesay.httpspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- devops-k8s-master/http-spring/src/test/java/com/jaesay/httpspring/HttpSpringApplicationTests.java
+++ devops-k8s-master/http-spring/src/test/java/com/jaesay/httpspring/HttpSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jaesay.demodocker;
+package com.jaesay.httpspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
