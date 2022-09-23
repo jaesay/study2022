517ff3b6be937e76d0b0cafc485daf3f4b8ed5a5
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-09-23 23:36:48 +0900

    junit5-dci 프로젝트 추가

---
+++ playground/junit5-dci/src/main/java/com/example/junit5dci/Junit5DciApplication.java
@@ -0,0 +1,13 @@
+package com.example.junit5dci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5DciApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Junit5DciApplication.class, args);
+  }
+
+}
---
+++ playground/junit5-dci/src/test/java/com/example/junit5dci/Junit5DciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junit5dci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5DciApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
