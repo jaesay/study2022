f98338d310979703d1edb561d78039c20bc9facf
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-10-13 22:11:23 +0900

    d2-jackson 프로젝트 생성

---
+++ playground/d2-jackson/src/main/java/com/example/d2jackson/D2JacksonApplication.java
@@ -0,0 +1,13 @@
+package com.example.d2jackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2JacksonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(D2JacksonApplication.class, args);
+  }
+
+}
---
+++ playground/d2-jackson/src/test/java/com/example/d2jackson/D2JacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.d2jackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2JacksonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
