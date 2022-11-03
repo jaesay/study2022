ebee2c8bfde343187b005c442daf8774a0f032fd
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-11-04 06:41:34 +0900

    @JsonAnyGetter 예제 추가

---
+++ playground/jackson-annotation-example/src/main/java/jsonformat/ExtendableBean.java
@@ -0,0 +1,24 @@
+package jsonformat;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExtendableBean {
+  public String name;
+  private Map<String, String> properties;
+
+  public ExtendableBean(String name) {
+    this.name = name;
+    properties = new HashMap<>();
+  }
+
+  @JsonAnyGetter
+  public Map<String, String> getProperties() {
+    return properties;
+  }
+
+  public void add(String key, String value) {
+    properties.put(key, value);
+  }
+}
---
+++ playground/jackson-annotation-example/src/test/java/jsonformat/ExtendableBeanTest.java
@@ -0,0 +1,23 @@
+package jsonformat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+class ExtendableBeanTest {
+
+  @Test
+  public void whenSerializingUsingJsonAnyGetter_thenCorrect() throws JsonProcessingException {
+
+    ExtendableBean bean = new ExtendableBean("My bean");
+    bean.add("attr1", "val1");
+    bean.add("attr2", "val2");
+
+    String result = new ObjectMapper().writeValueAsString(bean);
+
+    // @JsonAnyGetter 안쓸경우 "{"name":"My bean","properties":{"attr2":"val2","attr1":"val1"}}"
+    assertThat(result).isEqualTo("{\"name\":\"My bean\",\"attr2\":\"val2\",\"attr1\":\"val1\"}");
+  }
+}
