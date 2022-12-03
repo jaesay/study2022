9a7277387e57f0435990d7f32d8fc7dde85a0a9c
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-12-03 14:45:51 +0900

    Todo 추가하기

--- todo-app/todo-react-app/src/App.js
+++ todo-app/todo-react-app/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from "./Todo";
+import {useState} from "react";
+import {Container, List, Paper} from "@mui/material";
+import AddTodo from "./AddTodo";
 
 function App() {
+  const [items, setItems] = useState([
+      {
+        id: "0",
+        title: "Hello World 1",
+        done: true,
+      },
+      {
+        id: "1",
+        title: "Hello World 2",
+        done: false,
+      },
+  ]);
+
+  const addItem = (item) => {
+    item.id = "ID-" + items.length; // key를 위한 id
+    item.done = false;
+    setItems([...items, item]) // 새배열 생성(immutable), 리액트는 레퍼런스를 기준으로 재렌더링하기 때문에 새배열을 만들어줘야 한다.
+    console.log("items : ", items);
+  }
+
+  let todoItems = items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {items.map((item) => (
+              <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="md">
+        <AddTodo addItem={addItem} />
+        <div className="TodoList">{todoItems}</div>
+      </Container>
     </div>
   );
 }
---
+++ todo-app/todo-react-app/src/Todo.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react"
+import {Checkbox, InputBase, ListItem, ListItemText} from "@mui/material";
+
+const Todo = (props) => {
+  const [item, setItem] = useState(props.item);
+
+  return (
+    <ListItem>
+      <Checkbox checked={item.done} />
+      <ListItemText>
+        <InputBase
+          inputProps={{ "aria-label": "naked" }}
+          type="text"
+          id={item.id}
+          name={item.id}
+          value={item.title}
+          multiline={true}
+          fullWidth={true}
+        />
+      </ListItemText>
+    </ListItem>
+  );
+};
+
+export default Todo;
