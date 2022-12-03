0043679ddebe5172c9aa912fdc0f7862950c9041
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-12-03 15:37:33 +0900

    todo-app-backend 프로젝트 생성

---
+++ todo-app/todo-app-backend/src/test/java/com/example/todoappbackend/TodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
