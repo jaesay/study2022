99664e97b9eefaca915a868f21cdff0bc7fcbc44
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-12-03 18:16:49 +0900

    discount-alarm-app-backend 프로젝트 생성

---
+++ discount-alarm-app-backend/src/main/java/com/example/discountalarmappbackend/DiscountAlarmAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.discountalarmappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountAlarmAppBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiscountAlarmAppBackendApplication.class, args);
+  }
+
+}
---
+++ discount-alarm-app-backend/src/test/java/com/example/discountalarmappbackend/DiscountAlarmAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discountalarmappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountAlarmAppBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
