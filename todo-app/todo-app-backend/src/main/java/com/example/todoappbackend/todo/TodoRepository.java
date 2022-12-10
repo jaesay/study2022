5e7939014f8a913320a67155b92078d509576dbc
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-12-10 18:19:31 +0900

    to-app 삭제

--- todo-app/todo-app-backend/src/main/java/com/example/todoappbackend/todo/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.todoappbackend.todo;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<TodoEntity, String> {
-  List<TodoEntity> findByUserId(String userId);
-}
