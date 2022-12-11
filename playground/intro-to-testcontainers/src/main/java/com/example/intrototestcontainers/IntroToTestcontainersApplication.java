80eb24d32adbd300b82b5a4183523f1602d4c793
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-12-11 14:53:46 +0900

    intro-to-testcontainers 프로젝트 생성

---
+++ playground/intro-to-testcontainers/src/main/java/com/example/intrototestcontainers/IntroToTestcontainersApplication.java
@@ -0,0 +1,13 @@
+package com.example.intrototestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToTestcontainersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IntroToTestcontainersApplication.class, args);
+  }
+
+}
---
+++ playground/intro-to-testcontainers/src/test/java/com/example/intrototestcontainers/IntroToTestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intrototestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToTestcontainersApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
