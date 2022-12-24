893ab73a1c522be875fc7f4365ff6ab1eb109b27
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-12-24 19:43:14 +0900

    7. JUnit 5: 커스텀 태그

---
+++ inflearn-the-java-test/src/test/java/com/example/inflearnthejavatest/_01_junit5/_07_custom_tag/FastTest.java
@@ -0,0 +1,15 @@
+package com.example.inflearnthejavatest._01_junit5._07_custom_tag;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("fast")
+@Test
+public @interface FastTest {
+}
---
+++ inflearn-the-java-test/src/test/java/com/example/inflearnthejavatest/_01_junit5/_07_custom_tag/StudyTest.java
@@ -0,0 +1,24 @@
+package com.example.inflearnthejavatest._01_junit5._07_custom_tag;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.inflearnthejavatest._01_junit5.Study;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class StudyTest {
+
+  @DisplayName("스터디 만들기 \uD83D\uDE00")
+  @FastTest
+  void create_new_study() {
+    Study study = new Study(10);
+    assertThat(study.getLimit()).isGreaterThan(0);
+  }
+
+  @SlowTest
+  void create_new_study_again() {
+    System.out.println("test2");
+  }
+}
