fd4bfccde148685e99072336bf861aeb54edba50
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-12-26 23:23:16 +0900

    customer-client 프로젝트 생성

---
+++ playground/spring-cloud-contract-example/customer-client/src/test/java/com/example/customerclient/CustomerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerClientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
