8e31f60b1638da9ac71791cbd948a31c8d601753
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-01-06 23:41:02 +0900

    애노테이션 프로세서 실습

---
+++ the-java-code-manipulation/_05_annotation_processor/annomoja/src/main/java/org/example/App.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Moja moja = new MagicMoja();
+        System.out.println(moja.pullOut());
+    }
+}
